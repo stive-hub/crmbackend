0f6aae2dc6f6981675244cee502c904d92a07ed8
vivekj1795 <vivekj1795@gmail.com>
2020-01-30 11:19:43 +0530

    testing

--- ui/urls.py
+++ ui/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from ui.views import index, uilogout
+from ui.views import index, uilogout, test
 
 urlpatterns = [
     path('', index, name='index'),
+    path('test',test,name = 'test'),
     path('logout', uilogout, name='logout')    
 ]
